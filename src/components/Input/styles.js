537bc22e26ef76637ad858ff1d4bac0d6a514fba
Iana Karolina <ianakarolina@poli.ufrj.br>
2022-02-18 14:21:21 -0300

    input finalizado

---
+++ src/components/Input/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+
+export const InputText = styled.TextInput`
+    width: ${props=>props.width}px;
+    height: 50px;
+    background-color: #E5E5E5;
+    border-radius: 5px;
+    padding-left: 10px;
+    align-self: center;
+    margin-top: 10px;
+    margin-left:10px;
+`;
